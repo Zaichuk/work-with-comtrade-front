a5c39928028d92c5db70a6efe19bffb8b9f8cfc6
Ivan Zaichuk <zaichukip516@gmail.com>
2023-08-13 00:57:05 +0300

    add routes and breadcrumb on the ng-dynamic-breadcrumb lib

---
+++ src/app/comtrade/comtrade.component.ts
@@ -0,0 +1,66 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {CommunicationService} from "../communication.service";
+import {HttpService} from "../http.service";
+export class Measurement {
+  id: number;
+  ia: number;
+  ib: number;
+  ic: number;
+
+  constructor(id: number, ia: number, ib: number, ic: number) {
+    this.id = id;
+    this.ia = ia;
+    this.ib = ib;
+    this.ic = ic;
+  }
+}
+@Component({
+  selector: 'app-comtrade',
+  templateUrl: './comtrade.component.html',
+  styleUrls: ['./comtrade.component.css']
+})
+export class ComtradeComponent implements OnInit, OnDestroy{
+  title = 'work-with-comtrade-front';
+  file: any;
+  measurements: Measurement[] = [];
+  sub: Subscription[] = [
+    new Subscription(),
+    new Subscription(),
+  ];
+  isTableVisible = false;
+
+  constructor(
+      private transfer: CommunicationService,
+      private http: HttpService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.sub[0] = this.transfer.file$.subscribe((value) => {
+      this.file = value;
+      this.getMeasurements();
+    });
+  }
+
+  uploadFile(): void {
+    this.http.uploadFile(this.file);
+  }
+
+  getMeasurements(): void {
+    this.uploadFile();
+
+    this.sub[1] = this.http.getMeasurements().subscribe((value) => {
+      this.measurements = value;
+      this.transfer.addMeasurements(this.measurements);
+    });
+    this.isTableVisible = true;
+  }
+
+  ngOnDestroy(): void {
+    this.sub.forEach((el) => {
+          el.unsubscribe();
+        }
+    );
+  }
+}
